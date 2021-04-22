a55c6d698d2b8b25e1d018a9f315ae5669c44a61
lannparty <luanqtruong@gmail.com>
2021-04-21 22:05:48 -0700

    Multiple tickers

--- backtesting/more_liver_chunks.py
+++ backtesting/more_liver_chunks.py
@@ -32,17 +32,19 @@ def more_liver_test(tickers, signal):
         parsed[signal_sma_delta] = (parsed[ticker] - parsed[signal_sma]) / parsed[ticker]
         parsed = parsed[parsed[signal_sma].notna()]
 
-        black_swan[ticker] = parsed[parsed[signal_sma_delta] < 0].dropna()[signal_sma_delta].quantile(.3)
+        black_swan[ticker] = parsed[parsed[signal_sma_delta] < 0].dropna()[signal_sma_delta].quantile(.1)
 
     balance = 100000.00
     chunk = 5000.00
     shares = {}
     next_buy_price = {}
+    chunk_counter = {}
     buy_point = {}
 
     for ticker in tickers:
         shares[ticker] = 0
         next_buy_price[ticker] = 0
+        chunk_counter[ticker] = 5
 
     for row_index, row_value in parsed.iterrows():
         for ticker in tickers:
@@ -52,21 +54,33 @@ def more_liver_test(tickers, signal):
             shares_to_buy = math.floor(chunk / price)
 
             if shares[ticker] == 0 and balance > chunk:
-                if price < buy_point[ticker] and price > next_buy_price[ticker]:
+                if price < buy_point[ticker] and price > next_buy_price[ticker] and chunk_counter[ticker] > 0:
                     shares_to_buy = math.floor(chunk / price)
 
                     balance -= round((shares_to_buy * price), 2)
                     shares[ticker] += shares_to_buy
 
                     next_buy_price[ticker] = round(price + (price * .01), 2)
+                    chunk_counter[ticker] -= 1
                     parsed.loc[row_value.name, ticker + 'bought'] = price
-                    print("Balance", balance, "bought", shares_to_buy, "shares of", ticker, "at price", price, "now own", shares[ticker], "shares", row_index)
+                    print("Balance", balance, "bought", shares_to_buy, "shares of", ticker, "at price", price, "chunk number", 5 - chunk_counter[ticker], "now own", shares[ticker], "shares", row_index)
 
             elif shares[ticker] > 0:
-                if price > row_value[signal_sma] and shares[ticker] > 0:
+                if price > next_buy_price[ticker] and chunk_counter[ticker] > 0 and balance > chunk:
+
+                    balance -= round((shares_to_buy * price), 2)
+                    shares[ticker] += shares_to_buy
+
+                    next_buy_price[ticker] = round(price + (price * .01), 2)
+                    chunk_counter[ticker] -= 1
+                    parsed.loc[row_value.name, ticker + 'bought'] = price
+                    print("Balance", balance, "bought", shares_to_buy, "shares of", ticker, "at price", price, "chunk number", 5 - chunk_counter[ticker], "now own", shares[ticker], "shares", row_index)
+
+                elif price > row_value[signal_sma] and shares[ticker] > 0:
                     print("sold", shares[ticker], "shares of", ticker, "at", price, "per shares", "total", round((shares[ticker] * price), 2))
                     balance += round((shares[ticker] * price), 2)
                     shares[ticker] = 0
+                    chunk_counter[ticker] = 5            
                     next_buy_price[ticker] = 0
                     parsed.loc[row_value.name, ticker + 'sold'] = price
 
@@ -78,7 +92,7 @@ signal = str(200)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-tickers = ['ILMN']
+tickers = ['CVS']
 
 parsed = more_liver_test(tickers, signal)
 
