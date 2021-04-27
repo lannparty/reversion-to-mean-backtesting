a45b840556b71185896f81350c8650572c3d4b2e
lannparty <luanqtruong@gmail.com>
2021-04-27 13:19:09 -0700

    clean

--- backtesting/roi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `ls compare`
-do
-	echo $i >> out.file
-	cat compare/$i |tail -1 |awk '{print $4}' >> out.file
-done
--- backtesting/test.py
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `cat data/tickers.csv`
-do
-  python3 more_liver.py $i > data/test/$i.txt
-done
-
